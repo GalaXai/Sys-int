7eedc80ff0446c124e3b72dbef2a75d61e81dc39
eryk-tabis <s222765@student.uek.krakow.pl>
2024-03-23 20:29:31 +0100

    Fix typo

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from src.mrp import MRP
 def main():
     # TODO: Test values, need to be changed
     # długopis(wkład, obudowa(skuwka, obudowa główna))
-    wkład = MRP("Wkład", 3, 10, 3, 60)
+    wklad = MRP("Wkład", 3, 10, 3, 60)
     skuwka = MRP("Skuwka", 1, 5, 1, 30)
-    obudowa_część = MRP("Główna część obudowy", 2, 35, 1, 40)
-    obudowa = MRP("Obudowa", 1, 5, 1, 30, [skuwka, obudowa_część])
+    obudowa_czesc = MRP("Główna część obudowy", 2, 35, 1, 40)
+    obudowa = MRP("Obudowa", 1, 5, 1, 30, [skuwka, obudowa_czesc])
     ghp = GHP("Długopis", 1, 15, [
                         {"week": 5, "expected_demand": 20, "production": 28},
                         {"week": 7, "expected_demand": 40, "production": 30}],
-                        [wkład, obudowa])
+                        [wklad, obudowa])
     ghp.calculate_mrp()
 
 
