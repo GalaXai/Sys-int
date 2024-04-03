adf1eb39c61e27fa2fdca46d6db604bf37653cf1
GalaxUniv <afterhoursbilly@gmail.com>
2024-04-03 18:19:26 +0200

    TEMP main for testing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from src.ghp import GHP
-from src.mrp import MRP
+from src import MRPparser, GHP, MRP
 
 
 def main():
@@ -8,14 +7,37 @@ def main():
     wklad = MRP("Wkład", 3, 10, 3, 60)
     skuwka = MRP("Skuwka", 1, 5, 1, 30)
     obudowa_czesc = MRP("Główna część obudowy", 2, 35, 1, 40)
-    obudowa = MRP("Obudowa", 1, 5, 1, 30, [skuwka, obudowa_czesc])
-    ghp = GHP("Długopis", 1, 15, [
+    obudowa = MRP("Obudowa", 1, 5, 1, 30, mrp_array_lower_level = [skuwka, obudowa_czesc])
+    ghp = GHP("Długopis", 1, 15, 10,[
                         {"week": 5, "expected_demand": 20, "production": 28},
                         {"week": 7, "expected_demand": 40, "production": 30}],
                         [wklad, obudowa])
     ghp.calculate_mrp()
 
+def parser_main():
+    data = {"Wklad": [3, 10, 3, 60,],
+           "Skuwka": [1, 5, 1, 30],
+           "Główna część obudowy": [2, 35, 1, 40],
+           "Obudowa":[1, 5, 1, 30, 10,["Skuwka", "Główna część obudowy"]]}
+    parsed_data = MRPparser.parse(data)
+    print(parsed_data)
+
+    wklad = MRP("Wkład", 3, 10, 3, 60)
+    skuwka = MRP("Skuwka", 1, 5, 1, 30)
+    obudowa_czesc = MRP("Główna część obudowy", 2, 35, 1, 40)
+    obudowa = MRP("Obudowa", 1, 5, 1, 30, mrp_array_lower_level = [skuwka, obudowa_czesc])
+
+    p_ghp = GHP("Długopis", 1, 15, 10,[
+                        {"week": 5, "expected_demand": 20, "production": 28},
+                        {"week": 7, "expected_demand": 40, "production": 30}],
+                        [parsed_data["Wklad"], parsed_data["Obudowa"]])
+    ghp = GHP("Długopis", 1, 15, 10,[
+                        {"week": 5, "expected_demand": 20, "production": 28},
+                        {"week": 7, "expected_demand": 40, "production": 30}],
+                        [wklad, obudowa])
+    print(p_ghp.calculate_mrp() == ghp.calculate_mrp())
 
 if __name__ == "__main__":
-    main()
+    #main()
+    parser_main()
 
