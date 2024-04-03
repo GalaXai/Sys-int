5a5f694f163b0ff2c5b99fc2a23c2fd8a0968e2e
GalaxUniv <afterhoursbilly@gmail.com>
2024-04-03 19:19:17 +0200

    TMP comments

--- main.py
+++ main.py
@@ -15,17 +15,20 @@ def main():
     ghp.calculate_mrp()
 
 def parser_main():
+    #TODO: test values, that would come from GUI
     data = {"Wklad": [3, 10, 3, 60,],
            "Skuwka": [1, 5, 1, 30],
            "Główna część obudowy": [2, 35, 1, 40],
            "Obudowa":[1, 5, 1, 30, 10,["Skuwka", "Główna część obudowy"]]}
     parsed_data = MRPParser.parse(data)
     print(parsed_data)
-
+    
+    # Values from main to compare
     wklad = MRP("Wkład", 3, 10, 3, 60)
     skuwka = MRP("Skuwka", 1, 5, 1, 30)
     obudowa_czesc = MRP("Główna część obudowy", 2, 35, 1, 40)
     obudowa = MRP("Obudowa", 1, 5, 1, 30, mrp_array_lower_level = [skuwka, obudowa_czesc])
+
 
     p_ghp = GHP("Długopis", 1, 15, 10,[
                         {"week": 5, "expected_demand": 20, "production": 28},
@@ -35,6 +38,7 @@ def parser_main():
                         {"week": 5, "expected_demand": 20, "production": 28},
                         {"week": 7, "expected_demand": 40, "production": 30}],
                         [wklad, obudowa])
+
     print(p_ghp.calculate_mrp() == ghp.calculate_mrp())
 
 if __name__ == "__main__":
