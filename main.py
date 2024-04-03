b0225931693c869473cb5b884bd8bb5416b54d57
GalaxUniv <afterhoursbilly@gmail.com>
2024-04-03 19:38:58 +0200

    cleaned  main.py

--- main.py
+++ main.py
@@ -1,47 +1,19 @@
 from src import MRPParser, GHP, MRP
 
-
 def main():
     # TODO: Test values, need to be changed
     # długopis(wkład, obudowa(skuwka, obudowa główna))
-    wklad = MRP("Wkład", 3, 10, 3, 60)
-    skuwka = MRP("Skuwka", 1, 5, 1, 30)
-    obudowa_czesc = MRP("Główna część obudowy", 2, 35, 1, 40)
-    obudowa = MRP("Obudowa", 1, 5, 1, 30, mrp_array_lower_level = [skuwka, obudowa_czesc])
-    ghp = GHP("Długopis", 1, 15, 10,[
-                        {"week": 5, "expected_demand": 20, "production": 28},
-                        {"week": 7, "expected_demand": 40, "production": 30}],
-                        [wklad, obudowa])
-    ghp.calculate_mrp()
-
-def parser_main():
-    #TODO: test values, that would come from GUI
     data = {"Wklad": [3, 10, 3, 60,],
            "Skuwka": [1, 5, 1, 30],
            "Główna część obudowy": [2, 35, 1, 40],
            "Obudowa":[1, 5, 1, 30, 10,["Skuwka", "Główna część obudowy"]]}
-    parsed_data = MRPParser.parse(data)
-    print(parsed_data)
+    parsed_data = MRPParser.parser(data)
     
-    # Values from main to compare
-    wklad = MRP("Wkład", 3, 10, 3, 60)
-    skuwka = MRP("Skuwka", 1, 5, 1, 30)
-    obudowa_czesc = MRP("Główna część obudowy", 2, 35, 1, 40)
-    obudowa = MRP("Obudowa", 1, 5, 1, 30, mrp_array_lower_level = [skuwka, obudowa_czesc])
-
-
-    p_ghp = GHP("Długopis", 1, 15, 10,[
-                        {"week": 5, "expected_demand": 20, "production": 28},
-                        {"week": 7, "expected_demand": 40, "production": 30}],
-                        parsed_data)
     ghp = GHP("Długopis", 1, 15, 10,[
                         {"week": 5, "expected_demand": 20, "production": 28},
                         {"week": 7, "expected_demand": 40, "production": 30}],
-                        [wklad, obudowa])
-
-    print(p_ghp.calculate_mrp() == ghp.calculate_mrp())
+                        parsed_data)
+    ghp.calculate_mrp()
 
 if __name__ == "__main__":
-    #main()
-    parser_main()
-
+    main()
